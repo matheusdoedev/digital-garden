ef879d4c9b40b3b248896944dd5d209a259157ed
matheusdoedev <matheusdoe.dev@gmail.com>
2021-05-08 15:52:38 -0300

    :memo: chore: TypeScript exercise updated

--- Computer-Science/Technologies/TypeScript/code/01_Roman-Numerals-Decoder.ts
+++ Computer-Science/Technologies/TypeScript/code/01_Roman-Numerals-Decoder.ts
@@ -8,10 +8,50 @@ enum RomanNumericValues {
   M = 1000,
 }
 
-export function solution(roman: string): number {
-  let result: number;
+function getValue(roman: string): number {
+  switch (roman) {
+    case "I":
+      return RomanNumericValues.I;
+    case "V":
+      return RomanNumericValues.V;
+    case "X":
+      return RomanNumericValues.X;
+    case "L":
+      return RomanNumericValues.L;
+    case "C":
+      return RomanNumericValues.C;
+    case "D":
+      return RomanNumericValues.D;
+    case "M":
+      return RomanNumericValues.M;
+    default:
+      throw new Error("Roman symbol invalid");
+  }
+}
 
-  Array.from(roman).forEach((i) => {});
+function solution(roman: string): number {
+  let result = 0;
+  let previousValue: number;
+
+  Array.from(roman).forEach((i) => {
+    const value = getValue(i);
+
+    if (previousValue && previousValue < value) {
+      result += value - previousValue;
+    } else {
+      result += value;
+    }
+
+    previousValue = value;
+  });
+
+  console.log(result);
 
   return 0;
 }
+
+solution("XXI"); // 21
+solution("I"); // 1
+solution("IV"); // 4
+solution("MMVIII"); // 2008
+solution("MDCLXVI"); // 1666
